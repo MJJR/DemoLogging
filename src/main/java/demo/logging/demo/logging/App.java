6840fae04c8a6700958f539675707d216557cbe1
Redor <matthieu.redor@hotmail.fr>
2018-06-11 11:57:58 +0200

    Initialisation du premier TP DemoLogging

---
+++ src/main/java/demo/logging/demo/logging/App.java
@@ -0,0 +1,13 @@
+package demo.logging.demo.logging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
